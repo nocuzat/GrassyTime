6287644aa9f0a1c9b85ab2a221528a046865f430
Thulani Nocuza <nocuzat@gmail.com>
2022-06-08 00:31:41 +0200

    initial commit

---
+++ src/core/theme.tsx
@@ -0,0 +1,11 @@
+import { DefaultTheme } from 'react-native-paper';
+
+export const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#32cd32',
+    secondary: '#414757',
+    error: '#f13a59',
+  },
+};
